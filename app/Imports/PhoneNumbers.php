1fd2a573945fcad41e997679474a2a8928e74c0d
Matteo Meloni <matteomelonig@gmail.com>
2021-11-25 18:47:35 +0100

    PhoneNumbers import class, added progress

--- app/Imports/PhoneNumbers.php
+++ app/Imports/PhoneNumbers.php
@@ -3,12 +3,17 @@
 namespace App\Imports;
 
 use App\Models\PhoneNumber;
+use Illuminate\Console\OutputStyle;
 use Illuminate\Support\Str;
+use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithProgressBar;
 
-class PhoneNumbers implements ToModel, WithHeadingRow
+class PhoneNumbers implements ToModel, WithHeadingRow, WithProgressBar
 {
+    use Importable;
+
     public function model(array $row)
     {
         $data = [
@@ -29,10 +34,11 @@ class PhoneNumbers implements ToModel, WithHeadingRow
             $data = [
                 'mobile' => "27{$row['sms_phone']}",
                 'status' => PhoneNumber::CORRECTED,
-                'status_description' => 'ADDED INTERNATION PREFIX'
+                'status_description' => 'ADDED INTERNATIONAL PREFIX'
             ];
         }
 
         return new PhoneNumber($data);
     }
+
 }
